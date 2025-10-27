d2980215a49dad0818682c6790314e60f61ee2b8
Deadly Utopia <98151864+DeadlyUtopia@users.noreply.github.com>
2025-10-27 21:28:01 +0800

    Update inject_flag.sh

--- 信息收集_web12/docker/inject_flag.sh
+++ 信息收集_web12/docker/inject_flag.sh
@@ -5,7 +5,7 @@ new_number=$(python3 /app/generate_phone.py)
 # 步骤2：替换Hexo主题配置中的旧号码
 sed -i "s/952701145/$new_number/g" /app/themes/next/_config.yml
 
-# 步骤3：在容器启动时生成Hexo静态文件（符合你的要求）
+# 步骤3：在容器启动时生成Hexo静态文件
 cd /app && npx hexo clean && npx hexo generate && cp -r public/* /usr/share/nginx/html/
 
 # 步骤4：生成Nginx认证密码
